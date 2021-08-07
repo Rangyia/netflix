8802c835c97ec9e4661ef4a5d28652de43f03545
rangyia <jess.graham.dev@gmail.com>
2021-08-07 03:56:57 -0400

    Add express.js middleware to project

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-# nodde modules
-    / node_modules
+/* DEPENDENCIES */
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const path = require("path");
 
-# env information
-    .env
-    .sequelizerc
-    .vscode /
+/* ENVIRONMENTAL VARIABLES */
+const PORT = process.env.PORT || 5000;
 
-    app / package - lock.json
+/* CORS SECURITY - MIDDLE MAN */
+app.use(cors());
+app.use(express.json());
 
-app / yarn.lock
+/* STATIC CONTENT */
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/build")));
+}
+
+/* ROUTE MAPPER */
+
+
+/* ROUTER STACK */
+
+
+/* CATCH ALL */
+app.get("*", (req, res) => {
+    res.json("Invalid/url");
+});
+
+/* MAIN */
+app.listen(PORT, () => {
+    console.log(`server has started on port ${PORT}`);
+});
